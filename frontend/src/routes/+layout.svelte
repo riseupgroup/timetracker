f3445f9de9e7e2371b19edd2a97e5b995d331103
Untbu <mail@untbu.de>
2024-10-01 18:29:18 +0200

    add svelte-path-finder

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,24 +2,36 @@
     import "../app.css";
     import { page } from "$app/stores";
     import { goto } from "$app/navigation";
+    import { SveltePathFinder } from "svelte-path-finder";
 
-    const NO_LOGIN = ["", "login", "about"];
     let show: boolean = false;
 
+    let finder: SveltePathFinder | null;
+    async function getFinder(): Promise<SveltePathFinder> {
+        if (finder != null) return finder;
+        let res = await fetch("/svelte_path_finder.json");
+        if (res.ok) {
+            finder = new SveltePathFinder().fromJson(await res.json());
+            return finder;
+        } else {
+            alert(await res.text());
+            return null as any;
+        }
+    }
+
     page.subscribe(async (page) => {
-        if (page.url != null) {
-            let path = trimPath(page.url.pathname);
-            if (NO_LOGIN.includes(path)) {
-                show = true;
-            } else {
-                let user = await window.getUser();
-                if (user != null) {
-                    show = true;
-                } else {
-                    goto("/login?path=/" + path);
-                }
+        let path = trimPath(page.url.pathname);
+        let requireLogin = (await getFinder()).find(path);
+
+        if (requireLogin) {
+            let user = await window.getUser();
+            if (user == null) {
+                show = false;
+                goto("/login?path=/" + path, {replaceState: true});
+                return;
             }
         }
+        show = true;
     });
 
     function trimPath(path: string): string {
--- src/frontend.rs
+++ src/frontend.rs
@@ -1,12 +1,25 @@
 use {
     crate::user::SessionUser,
     actix_session::Session,
-    actix_web::{http::StatusCode, web, Error, HttpRequest, HttpResponse, Route},
+    actix_web::{get, http::{header::{self, HeaderValue}, Method, StatusCode}, web, Error, HttpRequest, HttpResponse, HttpResponseBuilder, Route},
 };
 
-const EXISTING: svelte_path_matcher::SveltePathMatcher<'static> =
-    svelte_path_matcher::build_from_filesystem!("./frontend/src/");
-const NO_LOGIN: &[&str] = &["", "login", "about"];
+const PATHS: svelte_path_finder::SveltePathFinder<'static> =
+    svelte_path_finder::build_from_filesystem!("./frontend/src/");
+
+const CACHE_CONTROL: &str = "public, immutable, max-age=86400"; // 60*60*24 = 86400
+
+lazy_static::lazy_static! {
+    static ref PATHS_JSON: String = serde_json::to_string(&PATHS).unwrap();
+    static ref CACHE_CONTROL_HEADER: HeaderValue = HeaderValue::from_str(CACHE_CONTROL).unwrap();
+}
+
+fn set_cache_header(path: &str, mut response: HttpResponse) -> HttpResponse {
+    if path.starts_with("_app/immutable") {
+        response.headers_mut().append(header::CACHE_CONTROL, CACHE_CONTROL_HEADER.clone());
+    }
+    response
+}
 
 #[derive(Debug, serde::Deserialize)]
 struct Query {
@@ -15,12 +28,18 @@ struct Query {
 
 #[cfg(debug_assertions)]
 mod debug {
-    use {super::*, actix_files::NamedFile};
+    use {super::*, actix_files::NamedFile, std::path::Path};
 
-    fn get_file(path: &str, req: &HttpRequest) -> HttpResponse {
-        match NamedFile::open(format!("./frontend/build/{path}")) {
-            Ok(file) => file.into_response(req),
-            Err(_) => match NamedFile::open("./frontend/build/index.html") {
+    pub fn get_file(path: &str, req: &HttpRequest) -> HttpResponse {
+        let full_path = format!("./frontend/build/{path}");
+        let full_path = Path::new(&full_path);
+        let mut file = None;
+        if full_path.is_file() {
+            file = NamedFile::open(full_path).ok();
+        }
+        match file {
+            Some(file) => set_cache_header(path, file.into_response(req)),
+            None => match NamedFile::open("./frontend/build/index.html") {
                 Ok(file) => {
                     let mut res = file.into_response(req);
                     *res.status_mut() = StatusCode::NOT_FOUND;
@@ -36,10 +55,13 @@ mod debug {
         session: Session,
         query: web::Query<Query>,
     ) -> Result<HttpResponse, Error> {
+        if req.method() != Method::GET {
+            return Ok(HttpResponse::NotFound().finish());
+        }
         let path = req.path().trim_matches('/');
 
-        match EXISTING.matches(path) {
-            true if SessionUser::try_from(&session).is_ok() => {
+        match PATHS.find(path) {
+            Some(_) if SessionUser::try_from(&session).is_ok() => {
                 if path == "login" {
                     Ok(HttpResponse::TemporaryRedirect()
                         .append_header(("location", query.path.as_deref().unwrap_or("/")))
@@ -48,15 +70,17 @@ mod debug {
                     Ok(get_file("index.html", &req))
                 }
             }
-            true if NO_LOGIN.contains(&path) => Ok(get_file("index.html", &req)),
-            true => Ok(HttpResponse::TemporaryRedirect()
+            Some(false) => Ok(get_file("index.html", &req)),
+            Some(true) => Ok(HttpResponse::TemporaryRedirect()
                 .append_header(("location", format!("/login?path={}", req.uri())))
                 .finish()),
-            false => Ok(get_file(path, &req)),
+            None => Ok(get_file(path, &req)),
         }
     }
 }
 
+#[cfg(debug_assertions)]
+pub use debug::get_file;
 #[cfg(debug_assertions)]
 use debug::serve_file;
 
@@ -72,12 +96,12 @@ mod release {
         static ref DATA: std::collections::HashMap<&'static str, static_files::Resource> = generate();
     }
 
-    pub fn get_file(path: &str) -> HttpResponse {
+    pub fn get_file(path: &str, _req: &HttpRequest) -> HttpResponse {
         match DATA.get(path) {
             Some(file) => {
                 let mut resp = HttpResponse::build(StatusCode::OK);
                 resp.content_type(file.mime_type);
-                resp.body(file.data)
+                set_cache_header(path, resp.body(file.data))
             }
             None => match DATA.get("index.html") {
                 Some(file) => {
@@ -95,10 +119,13 @@ mod release {
         session: Session,
         query: web::Query<Query>,
     ) -> Result<HttpResponse, Error> {
+        if req.method() != Method::GET {
+            return Ok(HttpResponse::NotFound().finish());
+        }
         let path = req.path().trim_matches('/');
 
-        match EXISTING.matches(path) {
-            true if SessionUser::try_from(&session).is_ok() => {
+        match PATHS.find(path) {
+            Some(_) if SessionUser::try_from(&session).is_ok() => {
                 if path == "login" {
                     Ok(HttpResponse::TemporaryRedirect()
                         .append_header((
@@ -107,21 +134,36 @@ mod release {
                         ))
                         .finish())
                 } else {
-                    Ok(get_file("index.html"))
+                    Ok(get_file("index.html", &req))
                 }
             }
-            true if NO_LOGIN.contains(&path) => Ok(get_file("index.html")),
-            true => Ok(HttpResponse::TemporaryRedirect()
+            Some(false) => Ok(get_file("index.html", &req)),
+            Some(true) => Ok(HttpResponse::TemporaryRedirect()
                 .append_header(("location", format!("/login?path={}", req.uri())))
                 .finish()),
-            false => Ok(get_file(path)),
+            None => Ok(get_file(path, &req)),
         }
     }
 }
 
 #[cfg(not(debug_assertions))]
+pub use release::get_file;
+#[cfg(not(debug_assertions))]
 use release::serve_file;
 
+#[get("svelte_path_finder.json")]
+async fn serve_paths() -> HttpResponse {
+    let cache_control = match cfg!(debug_assertions) {
+        true => "no-cache",
+        false => CACHE_CONTROL,
+    };
+    HttpResponseBuilder::new(StatusCode::OK)
+        .content_type("application/json")
+        .append_header((header::CACHE_CONTROL, cache_control))
+        .body(PATHS_JSON.as_str())
+}
+
 pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(serve_paths);
     cfg.default_service(Route::new().to(serve_file));
 }
